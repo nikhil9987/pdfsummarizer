f8db72c58579a86cd8d3eecf79390059af355df1
nikhil9987 <sawant.nikhil724@gmail.com>
2024-07-18 20:26:28 +0530

    Sixth commit

--- app.py
+++ app.py
@@ -37,9 +37,8 @@ def get_text_chunks(text):
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index",embeddings,allow_dangerous_deserialization=True)
-    # vector_store.load_local("faiss_index",embeddings,allow_dangerous_deserialization=True)
-# ,allow_dangerous_deserialization=True)
+    vector_store.save_local("faiss_index")
+   
 
 def get_conversational_chain():
 
@@ -65,7 +64,7 @@ def get_conversational_chain():
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
